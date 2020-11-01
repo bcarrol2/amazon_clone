b0d3a5f3fdf9edf4901d19abc032e22dba8a441f
bcarrol2 <blairacarroll@gmail.com>
2020-11-01 16:30:05 -0500

    products modified

--- src/Home.js
+++ src/Home.js
@@ -20,44 +20,44 @@ function Home() {
 
                 <Product
                     id="12342354"
-                    title="The Martian: A Novel Andy Weir Hardcover"
-                    price={23.99}
+                    title="2020 Playstation 4 PS4 1TB Slim Gaming Console w/Ghost Manta Fast Charging Station Dock"
+                    price={479.00}
                     rating={5}
-                    image="https://images-na.ssl-images-amazon.com/images/I/41vw+RmEi4L._SX327_BO1,204,203,200_.jpg"
+                    image="https://images-na.ssl-images-amazon.com/images/I/41HVaUKLTkL._AC_.jpg"
                 />
             </div>
             <div className="home__row">
                 <Product
                     id="12342355"
-                    title="The Martian: A Novel Andy Weir Hardcover"
-                    price={23.99}
+                    title="Fender Player Stratocaster Electric Guitar - Maple Fingerboard - Buttercream"
+                    price={699.00}
                     rating={5}
-                    image="https://images-na.ssl-images-amazon.com/images/I/41vw+RmEi4L._SX327_BO1,204,203,200_.jpg"
+                    image="https://images-na.ssl-images-amazon.com/images/I/610mei3tGrL._AC_SX679_.jpg"
                 />
 
                 <Product
                     id="12342356"
-                    title="The Martian: A Novel Andy Weir Hardcover"
-                    price={23.99}
+                    title="Donner DDP-100 88-Key Weighted Action Digital Piano, Beginner Bundle with Furniture Stand, Power Adapter, Triple Pedals, MP3 Function, Black"
+                    price={549.99}
                     rating={5}
-                    image="https://images-na.ssl-images-amazon.com/images/I/41vw+RmEi4L._SX327_BO1,204,203,200_.jpg"
+                    image="https://images-na.ssl-images-amazon.com/images/I/61vaf3CgRhL._AC_SX679_.jpg"
                 />
 
                 <Product
                     id="12342357"
-                    title="The Martian: A Novel Andy Weir Hardcover"
-                    price={23.99}
+                    title="It: A Novel Stephen King Hardcover"
+                    price={19.59}
                     rating={5}
-                    image="https://images-na.ssl-images-amazon.com/images/I/41vw+RmEi4L._SX327_BO1,204,203,200_.jpg"
+                    image="https://images-na.ssl-images-amazon.com/images/I/51tUePGFFlL._SX334_BO1,204,203,200_.jpg"
                 />
             </div>
             <div className="home__row">
                 <Product
                     id="12342358"
-                    title="The Martian: A Novel Andy Weir Hardcover"
-                    price={23.99}
+                    title="LG 34WN80C-B 34 inch 21:9 Curved UltraWide WQHD IPS Monitor with USB Type-C Connectivity sRGB 99% Color Gamut and HDR10 Compatibility, Black (2019)"
+                    price={549.99}
                     rating={5}
-                    image="https://images-na.ssl-images-amazon.com/images/I/41vw+RmEi4L._SX327_BO1,204,203,200_.jpg"
+                    image="https://images-na.ssl-images-amazon.com/images/I/81WBbFOEHwL._AC_SL1500_.jpg"
                 />
             </div>
         </div>
